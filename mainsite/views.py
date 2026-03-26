1cce7425a8272da4fd0563313a4f6e4d984f054b
Bernie Laird <asovace@gmail.com>
2026-03-26 13:13:07 -0500

    added functionality

--- mainsite/views.py
+++ mainsite/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "mainsite/index.html")
+
+def about(request):
+    return render(request, "mainsite/about.html")
+
+def projects(request):
+    return render(request, "mainsite/projects.html")
+
+def contact(request):
+    return render(request, "mainsite/contact.html")
