ec559f6cd0d05d8bc2ff2c24c808d6b1feda08be
Bernie Laird <asovace@gmail.com>
2026-03-28 14:19:21 -0500

    added transition

---
+++ mainsite/static/mainsite/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", function(e) {
+            if (this.hostname === window.location.hostname) {
+                e.preventDefault();
+                document.body.classList.add("fade-out");
+                setTimeout(() => {
+                    window.location = this.href;
+                }, 400);
+            }
+        });
+    });
+});
